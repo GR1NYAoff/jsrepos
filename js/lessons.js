b73b8f7fc6b0257e5414998a4f1ac4cf70876f4b
= <=>
2022-03-08 19:24:21 +0200

    arrays in js lesson

--- js/lessons.js
+++ js/lessons.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const arr = [1, 2, 4, 34, 32, 19];
+
+let numComparer = function(a,b) {
+      return a-b;
+};
+
+arr.sort(numComparer);
+
+for (let item of arr) {
+      console.log(item);
+}
+
+arr.forEach(function(item, index, arr) {
+      console.log(`${index}: ${item} in ${arr}`);
+});
